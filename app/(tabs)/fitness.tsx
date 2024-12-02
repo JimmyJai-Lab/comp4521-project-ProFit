701ca9137dc201eaa2b5380b5d56e91f4d8cc680
Oscarw212 <wongch212@gmail.com>
2024-12-02 08:16:52 +0800

    adding exercise record successfully and modify the fitness frontend

--- app/(tabs)/fitness.tsx
+++ app/(tabs)/fitness.tsx
@@ -4,7 +4,6 @@ import * as React from 'react';
 import CalendarStrip from 'react-native-calendar-strip';
 import { FlashList } from "@shopify/flash-list";
 import Checkbox from 'expo-checkbox';
-import { Button } from 'react-native-paper';
 import { router } from 'expo-router';
 import email from './fitness';
 import { ListItemSubtitle } from '@rneui/base/dist/ListItem/ListItem.Subtitle';
@@ -57,21 +56,21 @@ export default function FitnessScreen() {
       name: "chest butterfly",
       weight: 30,
       set: 4,
-      rep: 10, 
+      rep: 10,
       image: require("../../assets/images/chest butterfly.webp"),
       checked: false,
     },
   ];
   const [isChecked, setChecked] = React.useState(DATA);
 
-  const handleChange = (id : any) => {
+  const handleChange = (id: any) => {
     //alert(Object.values(isChecked[id]));
     let temp = isChecked.map((product) => {
-      if (id === product.id){
+      if (id === product.id) {
         return { ...product, checked: !product.checked };
       }
-      return product; 
-    });  
+      return product;
+    });
     setChecked(temp);
   };
   // const Item = ({data}: {data: Exercises}) => (
@@ -86,152 +85,163 @@ export default function FitnessScreen() {
   //     <Text style={{fontSize: 24}}>{data.name}</Text>
   //   </View>
   // )
-  const renderItem = ({item}: {item: Exercises}) => {
-      return (
-        <View style= {{flexDirection: 'row',}}>
-          <Image 
-            source={item.image}
-            style = {{ 
-              height: 80,
-              width: 80,}}
-          />
-          <View>
-            <View style= {{flexDirection: 'row',}}>
-             <Text style={{padding: 10, fontSize: 20, fontWeight: 'bold'}}>{item.name}</Text>
-             <Text style={{color: "#808080", padding: 13}}>Weight: {String(item.weight)} kg</Text>
-            </View>
-            <Text style={{color: "#808080", padding: 13}}>Volume: {String(item.set)} sets x {String(item.rep)} reps </Text>
+  const renderItem = ({ item }: { item: Exercises }) => {
+    return (
+      <View style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#eee'
+      }}>
+        <Image
+          source={item.image}
+          style={{
+            height: 60,
+            width: 60,
+            borderRadius: 5
+          }}
+        />
+        <View style={{ flex: 1, paddingHorizontal: 10 }}>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            <Text style={{ fontSize: 16, fontWeight: 'bold' }}>{item.name}</Text>
+            <Text style={{ color: "#808080" }}>Weight: {String(item.weight)} kg</Text>
           </View>
-          <Checkbox
-            style={{margin: 15, padding: 10, }}
-            value={isChecked[item.id].checked}
-            onValueChange={() => {handleChange(item.id);}}   
-          />
+          <Text style={{ color: "#808080", marginTop: 5 }}>
+            Volume: {String(item.set)} sets x {String(item.rep)} reps
+          </Text>
         </View>
-      )
-  }
-  return (
-    <View style={styles.container}>
-
-      <CalendarStrip
-        calendarAnimation={{type: 'sequence', duration: 30}}
-        daySelectionAnimation={{type: 'border', duration: 200, borderWidth: 1, borderHighlightColor: 'white'}}
-        style={{height: 100, paddingTop: 30, paddingBottom: 10}}
-        calendarHeaderStyle={{color: 'white',fontSize:10}}
-        calendarColor={'#7743CE'}
-        dateNumberStyle={{color: 'white',fontSize:10}}
-        dateNameStyle={{color: 'white',fontSize:10}}
-        highlightDateNumberStyle={{color: 'yellow',fontSize:10}}
-        highlightDateNameStyle={{color: 'yellow',fontSize:10}}
-        disabledDateNameStyle={{color: 'grey',fontSize:10}}
-        disabledDateNumberStyle={{color: 'grey',fontSize:10}}
-        iconContainer={{flex: 0.1,fontSize:10}}
-     />
-
-      {/* <ScrollView> */}
-      <View style={styles.summarycontainer}>
-        <View style={{alignItems: 'center'}}>
-          <Text style={{fontSize: 30, fontWeight: 'bold'}}>Summary</Text>
-        </View>
-        <View style={{paddingLeft: 40}}>
-          <Text style={styles.heading}>Duration</Text>
-        </View>
-        <View style={{flexDirection:'row'}}>
-          <View>
-            <Text style={{paddingHorizontal: 40, fontSize: 25, color: 'white', fontWeight: 'bold'}}>{minutes}</Text>
-          </View>
-          <View>
-            <Text style={styles.text}>mins</Text>
-          </View>
-          <View>
-            <Text style={{paddingHorizontal: 30, fontSize: 25, color: 'white', fontWeight: 'bold'}}>{average_minutes}</Text>
-          </View>
-          <View>
-            <Text style={styles.text}>mins daily average</Text>
-          </View>
-        </View>
-        <View style={{flexDirection:'row'}}>
-          <View>
-            <View >
-              <Text style={[styles.heading, {paddingLeft: 40}]}>Set</Text>
-            </View>
-            <View style={{flexDirection:'row'}}>
-              <View>
-                <Text style={{paddingHorizontal: 40, fontSize: 25, color: 'white', fontWeight: 'bold'}}>{num_of_sets}</Text>
-              </View>
-              <View>
-                <Text style={styles.text}>sets</Text>
-              </View>
-            </View>
-          </View>
-          <View>
-            <View >
-              <Text style={[styles.heading, {paddingLeft: 40}]}>Volume</Text>
-            </View>
-            <View style={{flexDirection:'row'}}>
-              <View>
-                <Text style={{paddingHorizontal: 40, fontSize: 25, color: 'white', fontWeight: 'bold'}}>{volume}</Text>
-              </View>
-              <View>
-                <Text style={styles.text}>kgs</Text>
-              </View>
-            </View>
-          </View>
-        </View>
-      </View>
-
-
-      <View style={[styles.addingcontainer, {flexDirection: 'row', padding: 20}]}>
-        <View style={{alignItems: 'center', justifyContent: 'center'}}>
-          <Text style={[styles.heading, {paddingLeft: 20}]}>Today's Exercises</Text>
-        </View>
-        <View style= {{paddingLeft: 50}}>
-              <TouchableOpacity style= {{backgroundColor: '#75E6DA', borderRadius: 20, height: 50, width: 100, alignItems: 'center', justifyContent: 'center'}} onPress={() => router.navigate('/addexercise_api')}>
-                <Text style={{fontWeight:'bold',fontSize:20,color: 'black',padding:4}}>Add Set</Text>
-              </TouchableOpacity>
-        </View>
-      </View>
-
-      <ScrollView>
-      <View style={styles.exercisecontainer}>
-        <FlashList
-          data={DATA}
-          renderItem= {renderItem}
-          estimatedItemSize={3000}
+        <Checkbox
+          style={{ margin: 10 }}
+          value={isChecked[item.id].checked}
+          onValueChange={() => { handleChange(item.id); }}
         />
       </View>
+    );
+  };
+  return (
+    <View style={styles.container}>
+      <CalendarStrip
+        calendarAnimation={{ type: 'sequence', duration: 30 }}
+        daySelectionAnimation={{ type: 'border', duration: 200, borderWidth: 1, borderHighlightColor: 'white' }}
+        style={{ height: 80, paddingTop: 20, paddingBottom: 10 }}
+        calendarHeaderStyle={{ color: 'white', fontSize: 10 }}
+        calendarColor={'#7743CE'}
+        dateNumberStyle={{ color: 'white', fontSize: 10 }}
+        dateNameStyle={{ color: 'white', fontSize: 10 }}
+        highlightDateNumberStyle={{ color: 'yellow', fontSize: 10 }}
+        highlightDateNameStyle={{ color: 'yellow', fontSize: 10 }}
+        disabledDateNameStyle={{ color: 'grey', fontSize: 10 }}
+        disabledDateNumberStyle={{ color: 'grey', fontSize: 10 }}
+        iconContainer={{ flex: 0.1, fontSize: 10 }}
+      />
 
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.summarycontainer}>
+          <View style={{ alignItems: 'center' }}>
+            <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Summary</Text>
+          </View>
+          <View style={{ paddingLeft: 40 }}>
+            <Text style={styles.heading}>Duration</Text>
+          </View>
+          <View style={{ flexDirection: 'row' }}>
+            <View>
+              <Text style={{ paddingHorizontal: 40, fontSize: 25, color: 'white', fontWeight: 'bold' }}>{minutes}</Text>
+            </View>
+            <View>
+              <Text style={styles.text}>mins</Text>
+            </View>
+            <View>
+              <Text style={{ paddingHorizontal: 30, fontSize: 25, color: 'white', fontWeight: 'bold' }}>{average_minutes}</Text>
+            </View>
+            <View>
+              <Text style={styles.text}>mins daily average</Text>
+            </View>
+          </View>
+          <View style={{ flexDirection: 'row' }}>
+            <View>
+              <View >
+                <Text style={[styles.heading, { paddingLeft: 40 }]}>Set</Text>
+              </View>
+              <View style={{ flexDirection: 'row' }}>
+                <View>
+                  <Text style={{ paddingHorizontal: 40, fontSize: 25, color: 'white', fontWeight: 'bold' }}>{num_of_sets}</Text>
+                </View>
+                <View>
+                  <Text style={styles.text}>sets</Text>
+                </View>
+              </View>
+            </View>
+            <View>
+              <View >
+                <Text style={[styles.heading, { paddingLeft: 40 }]}>Volume</Text>
+              </View>
+              <View style={{ flexDirection: 'row' }}>
+                <View>
+                  <Text style={{ paddingHorizontal: 40, fontSize: 25, color: 'white', fontWeight: 'bold' }}>{volume}</Text>
+                </View>
+                <View>
+                  <Text style={styles.text}>kgs</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+        </View>
 
+        <View style={styles.addingcontainer}>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding: 10 }}>
+            <Text style={styles.heading}>Today's Exercises</Text>
+            <TouchableOpacity
+              style={{
+                backgroundColor: '#75E6DA',
+                borderRadius: 20,
+                padding: 10,
+                minWidth: 100,
+                alignItems: 'center'
+              }}
+              onPress={() => router.navigate('/addexercise_api')}>
+              <Text style={{ fontWeight: 'bold', fontSize: 16, color: 'black' }}>Add Set</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View style={styles.exercisecontainer}>
+          <FlashList
+            data={DATA}
+            renderItem={renderItem}
+            estimatedItemSize={100}
+            scrollEnabled={false}
+          />
+        </View>
       </ScrollView>
-
-      <Button mode="elevated" style ={styles.fab} labelStyle={{padding: 10, fontSize: 20, fontWeight: 'bold'}}>
-        Add Template
-      </Button>
-
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
     backgroundColor: '#FFFFFF',
   },
-  summarycontainer:{
-    flex: 6,
+  summarycontainer: {
     backgroundColor: '#FFC0CB',
-    //backgroundColor: '#FFFFFF',
     borderRadius: 10,
+    margin: 10,
+    padding: 10,
   },
-  addingcontainer:{
-    flex: 2,
+  addingcontainer: {
     backgroundColor: '#ADD8E6',
     borderRadius: 10,
+    margin: 10,
+    padding: 10,
   },
-  exercisecontainer:{
-    flex: 3,
+  exercisecontainer: {
     backgroundColor: '#E7DDFF',
     borderRadius: 10,
+    margin: 10,
+    minHeight: 200,
+    marginBottom: 20,
   },
   text: {
     color: '#000000',
@@ -241,12 +251,5 @@ const styles = StyleSheet.create({
     color: '#000000',
     fontSize: 25,
     fontWeight: 'bold',
-  },
-  fab: {
-    position: 'absolute',
-    margin: 16,
-    alignSelf: 'center',
-    bottom: "5%",
-    fontSize: 20,
   },
 });
